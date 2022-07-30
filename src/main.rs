b8fe36cce3d2f0f9221c7cea8d91a7803af83b5d
Timothy White <Tim@timothyhitesMBP.lan>
2022-07-29 23:17:40 -0400

    first commit?

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use std::io;
+
 fn main() {
-    let mut x = 5;
+    let x = 5;
     println!("The value of the x is {x}");
-    x = 6; 
-    println!("The value of the x is {x}");
+    let x = x + 1; 
+    {
+        let x = x * 2;
+        println!("The value of the x in the inner scope is {x}");
+    }
+    println!("The value of the x in the outer scope is {x}");
+    let mut spaces = String::new();
+    println!("input as many spaces as you want to have counted.");
+    io::stdin()
+        .read_line(&mut spaces)
+        .expect("Error reading input");
+    println!("The number of spaces in the spaces variable is {spaces}");
 }
